26fdd6c63541859bd0902ebfe69c3fe947c39111
Volodymyr <47578369+VolodymyrPerun@users.noreply.github.com>
2021-02-07 15:12:01 +0200

    refactored fo correct filtration

--- constants/productSection.enum.js
+++ constants/productSection.enum.js
@@ -5,6 +5,12 @@ module.exports = {
     CHEESE: 4,
     HALF: 5,
     HOT_ROLES: 6,
-    MEAT: 7,
-    PROMOTIONAL: 8
+    SUSHI: 7,
+    CHAINS: 8,
+    MISO_SOUPS: 9,
+    SALADS: 10,
+    DESSERTS: 11,
+    DRINKS: 12,
+    NO_SECTION_MEAL: 13,
+    PROMOTIONAL: 14
 };
--- constants/productType.enum.js
+++ constants/productType.enum.js
@@ -1,11 +1,7 @@
 module.exports = {
     PIZZA: 1,
-    ROLES: 2,
-    SUSHI: 3,
-    CHAINS: 4,
-    MISO_SOUPS: 5,
-    SALADS: 6,
-    DESSERTS: 7,
-    DRINKS: 8,
-    SUPPLEMENTS: 9
+    SUSHI_AND_ROLES: 2,
+    SOUPS_AND_SALADS: 3,
+    DESSERTS_AND_DRINKS: 4,
+    SUPPLEMENTS: 5
 };
