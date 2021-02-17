150e4381cf97581a80335df19b55fd5e9e654c8f
Volodymyr <47578369+VolodymyrPerun@users.noreply.github.com>
2021-02-17 15:14:56 +0200

    Update getProductById.service.js

--- service/productService/getProductById.service.js
+++ service/productService/getProductById.service.js
@@ -1,12 +1,33 @@
 const db = require('../../dataBase').getInstance();
 
-const {DB_TABLE_NAME: {PRODUCT}} = require('../../constants');
+const {DB_TABLE_NAME: {PRODUCT, PRODUCT_TYPE, PRODUCT_STATUS, PRODUCT_SIZE, PRODUCT_SECTION}} = require('../../constants');
 
 module.exports = async options => {
     const ProductModel = await db.getModel(PRODUCT);
+    const ProductTypeModel = await db.getModel(PRODUCT_TYPE);
+    const ProductStatusModel = await db.getModel(PRODUCT_STATUS);
+    const ProductSizeModel = await db.getModel(PRODUCT_SIZE);
+    const ProductSectionsModel = await db.getModel(PRODUCT_SECTION);
 
     return ProductModel.findByPk(options, {
         raw: true,
-        // attributes: ['status_id']
+        include: [
+            {
+                model: ProductTypeModel,
+                attributes: ['type']
+            },
+            {
+                model: ProductStatusModel,
+                attributes: ['status']
+            },
+            {
+                model: ProductSizeModel,
+                attributes: ['size']
+            },
+            {
+                model: ProductSectionsModel,
+                attributes: ['section']
+            }
+        ]
     });
 };
